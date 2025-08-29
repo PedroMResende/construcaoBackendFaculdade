df068a274b1965587b705c2394a94e254616db15
Pedro Resende <professorpedroresende@gmail.com>
2025-08-28 21:41:20 -0300

    aula4

---
+++ aulas/aula4/src/calculadora.js
@@ -0,0 +1,16 @@
+
+
+function soma(a,b) { 
+    return (a+b); 
+}
+
+function multiplicacao(a,b) { 
+    return a*b; 
+}
+
+function divisao(a,b) { 
+    if(b===0) throw Error("divisão por zero")
+}
+
+module.exports={soma, multiplicacao, divisao} ; 
+
