1f6e0d24e3b4d1a782df3eb96a0b16097db7eba5
Pedro Resende <professorpedroresende@gmail.com>
2025-11-11 21:13:37 -0300

    Pratica09

---
+++ praticas/pratica09/routes/apidocsRouter.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const swaggerUi = require('swagger-ui-express'); 
+const fs = require('fs'); 
+const YAML = require('yaml'); 
+
+const router = express.Router(); 
+const file = fs.readFileSync('./swagger.yaml', 'utf-8');
+const swaggerDocument = YAML.parse(file); 
+
+
+router.use('/', swaggerUi.serve); 
+
+router.get('/', swaggerUi.setup(swaggerDocument)); 
+
+module.exports = router;
+
+
